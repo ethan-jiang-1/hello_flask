bff83a21fbca90cac3c810efc4fd59f202b4efec
ethan <ethan1.jiang@gmail.com>
2015-05-20 17:57:40 +0800

    debug

--- ws1.py
+++ ws1.py
@@ -1,7 +1,9 @@
 #!flask/bin/python
 from flask import Flask, jsonify
-from flask import abort
 from flask import make_response
+from flask import abort
+from flask import request
+import pdb
 
 app = Flask(__name__)
 
@@ -35,18 +37,66 @@ def get_task(task_id):
 	return jsonify({'task': task[0]})
 
 
-@app.route('/todo/api/v1.0/help', methods=['GET'])
-def help():
-	"""Print available functions."""
-	func_list = {}
-	for rule in app.url_map.iter_rules():
-		if rule.endpoint != 'static' : 
-			func_list[rule.rule] = app.view_functions[rule.endpoint].__doc__
-	return jsonify(func_list)
 
 @app.errorhandler(404)
 def not_find(error):
 	return make_response(jsonify({'error': 'Not found'}),404)
+
+
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+@app.route('/todo/api/v1.0/help', methods=['GET'])
+def help():
+	"""Print available functions."""
+	func_list = {}
+	pdb.set_trace()
+	print app.url_map.iter_rules()
+	for rule in app.url_map.iter_rules():
+		if rule.endpoint != 'static' : 
+			func_list[rule.rule] = app.view_functions[rule.endpoint].__doc__
+	return jsonify(func_list)    
 
 if __name__ == '__main__':
     app.run(debug=True)
